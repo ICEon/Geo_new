53954e2f3322ab681bcc1e5769d21ae1a1eca41c
ICEon <matrix_eon@yahoo.com>
2017-05-08 10:39:21 -0500

    Uno

---
+++ js/acciones.js
@@ -0,0 +1,13 @@
+// JavaScript Document
+
+$(document).ready(function(e) {
+	alert ("ready");
+document.addEventListener("deviceready", onDeviceReady, false);
+
+});
+
+
+function onDeviceReady() {
+ alert ("ready");
+}
+
